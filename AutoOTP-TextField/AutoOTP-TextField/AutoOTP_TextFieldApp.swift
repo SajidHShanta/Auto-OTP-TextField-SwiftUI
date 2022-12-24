ce2b67100093062ce86b2d5f5cfe131e83f08a40
sajid <sajidhasan673@gmail.com>
2022-12-24 06:34:51 +0600

    create app base

---
+++ AutoOTP-TextField/AutoOTP-TextField/AutoOTP_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOTP_TextFieldApp.swift
+//  AutoOTP-TextField
+//
+//  Created by Sajid Shanta on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AutoOTP_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoOTP-TextField/AutoOTP-TextField/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AutoOTP-TextField
+//
+//  Created by Sajid Shanta on 24/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
